5530fcbe2c61a35a33527ce80a7ded833a76a237
Artemiy <los28.2001@mail.ru>
2021-11-08 16:07:51 +0300

    speed time as in F12

--- JS/testJS.js
+++ JS/testJS.js
@@ -1,22 +1,28 @@
-kkk = (function getSpeedInfo(){
+var startTime = (function getSpeedInfo(){
     return Date.now();
 })();
 
-window.onload = function dacha(){
-    var SpeedInfo = document.getElementById("SpeedTest");
-    var loadingTime = new Date().getTime();
-    loadingTime -= kkk;
-    SpeedInfo.innerHTML += loadingTime + ' milliseconds'
+window.onload = function speedTimeandLocation(){
+    //IIFE variant
+    var speedInfo = document.getElementById("SpeedTest");
+    var loadedTime = new Date().getTime();
+    loadedTime -= startTime;
+    speedInfo.innerHTML += loadedTime + ' milliseconds'
+
+    //window.performance variant
+    let time = window.performance
+    let pageloadtime = time.timing.loadEventStart - time.timing.navigationStart;
+    speedInfo.innerHTML += "    Другой метод говроит, что грузится за: " + pageloadtime + " милисекунд";
 
     locationObj = document.location;
     var elem = document.getElementsByClassName("nav-href");
     for (var i = 0; i< elem.length; i++) {
         var lookLink = elem.item(i)
-        console.log(lookLink.href);
+            // || (lookLink.href === locationObj.href + "index.html")
         if (lookLink.href === locationObj.href) {
             lookLink.classList.add("activeLink")
-            console.log("yes");
         }
     }
+
 }
 
